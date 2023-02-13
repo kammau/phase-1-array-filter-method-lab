ce9903437cd7b88621e3a8e5566d93e9f6e8d3e3
Kamryn Smith <programersign@gmail.com>
2023-02-13 10:20:48 -0800

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+
+function filter(collection, cb) {
+    const newCollection = [];
+
+    for (const user of collection) {
+        if (cb(user)) {
+            newCollection.push(user);
+        }
+    }
+    return newCollection
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function findMatching(drivers, driverToFind) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === driverToFind.toLowerCase()
+    })
+  }
+
+  function fuzzyMatch(drivers, driverToFind) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, driverToFind.length) === driverToFind.toLowerCase()
+    })
+  }
+
+function matchName(drivers, driverToFind) {
+    return drivers.filter(function (driver) {
+        return driver.name === driverToFind
+    })
+}
+
